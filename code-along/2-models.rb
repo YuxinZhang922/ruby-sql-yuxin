1b45bf29e59a65611eb41c5bbd9150f7826cf25c
Yuxin Zhang <Yuxinzhang2024@u.northwestern.edu>
2024-04-26 15:19:59 +0000

    2

--- code-along/2-models.rb
+++ code-along/2-models.rb
@@ -13,15 +13,40 @@ Company.destroy_all
 
 # 1a. check out the schema file
 # 1b. check out the model file
+puts "There are #{Company.all.count} companies"
 
 # 2. insert new rows in companies table
+new_company = Company.new
+new_company["name"] = "Apple"
+new_company["city"] = "Chicago"
+new_company["state"] = "IL"
+new_company["url"] = "http://ss"
+new_company.save
+p new_company
+
+company = Company.new
+company["name"] = "XOM"
+company["city"] = "NY"
+company["state"] = "NY"
+company["url"] = "http://aa"
+company.save
+p company
+
+
+puts "There are #{Company.all.count} companies"
 
 # 3. query companies table to find all row with California company
+all_companies = Company.all
+p all_companies
 
 # 4. query companies table to find single row for Apple
-
+c_company = Company.where({"state"=>"IL"})
+p c_company
 # 5. read a row's column value
-
+apple = Company.find_by({"name" => "Apple"})
+p apple
+p apple["name"]
 # 6. update a row's column value
+
 
 # 7. delete a row
